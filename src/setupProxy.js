dbf2da9854ef9860bc7bdcab2997ad9fbbdcceb7
kx9x <kx9x@protonmail.com>
2022-01-24 17:15:52 -0800

    cors anywhere

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,14 +4,14 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function (app) {
-    app.use(
-        '/fantom',
-        createProxyMiddleware({
-          target: 'https://safe.fantom.network/',
-          changeOrigin: true,
-          pathRewrite: {
-              [`^/fantom`]: '',
-          }
+    app.use("/manifest.json", function (req, res, next) {
+        res.set({
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "GET",
+            "Access-Control-Allow-Headers":
+                "X-Requested-With, content-type, Authorization",
         })
-      );
+
+        next()
+    });
 }
